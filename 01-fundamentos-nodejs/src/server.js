79ea24fef81353caae4ed430284f2709d8ff3638
Alexandre Maeda <ale.b.maeda@gmail.com>
2023-04-01 15:58:11 -0300

    firt lesson about http server

---
+++ 01-fundamentos-nodejs/src/server.js
@@ -0,0 +1,26 @@
+import http from "node:http";
+
+const users = [];
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-Type", "application/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "User 1",
+    });
+
+    return res.writeHead(201).end("User added.");
+  }
+
+  return res.writeHead(404).end();
+});
+
+server.listen(3333);
