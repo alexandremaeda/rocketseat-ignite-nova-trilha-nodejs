3f5a0713ab7d120fbbb08d6caa0ddf59205e6643
Alexandre Maeda <ale.b.maeda@gmail.com>
2023-04-01 17:45:40 -0300

    Converting buffer into json

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -2,8 +2,20 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (err) {
+    req.body = null;
+  }
 
   if (method === "GET" && url === "/users") {
     return res
@@ -12,9 +24,12 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: "User 1",
+      name: name,
+      email: email,
     });
 
     return res.writeHead(201).end("User added.");
