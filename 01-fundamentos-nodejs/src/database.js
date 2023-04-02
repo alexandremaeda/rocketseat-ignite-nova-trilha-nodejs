5292e1e4fd2198ad3744d56f80561b7c467a25dc
Alexandre Maeda <ale.b.maeda@gmail.com>
2023-04-02 13:10:50 -0300

    Added update route and method

--- 01-fundamentos-nodejs/src/database.js
+++ 01-fundamentos-nodejs/src/database.js
@@ -24,7 +24,6 @@ export class Database {
 
     return data;
   }
-
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
       this.#database[table].push(data);
@@ -36,12 +35,19 @@ export class Database {
 
     return data;
   }
-
   delete(table, id) {
     const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
     if (rowIndex > -1) {
       this.#database[table].splice(rowIndex, 1);
+      this.#persist();
+    }
+  }
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data };
       this.#persist();
     }
   }
--- 01-fundamentos-nodejs/src/routes.js
+++ 01-fundamentos-nodejs/src/routes.js
@@ -44,4 +44,19 @@ export const routes = [
       return res.writeHead(204).end();
     },
   },
+  {
+    method: "PUT",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { name, email } = req.body;
+
+      database.update("users", id, {
+        name,
+        email,
+      });
+
+      return res.writeHead(204).end();
+    },
+  },
 ];
